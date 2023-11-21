a161f65730a252e06ff9d1e734f9987259b2f128
JSH <king601143@gmail.com>
2023-11-22 04:36:47 +0900

    b1

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,31 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
---
+++ src/main/java/umc/spring/domain/Store.java
@@ -0,0 +1,30 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 40)
+    private String address;
+
+    private Float score;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+}
